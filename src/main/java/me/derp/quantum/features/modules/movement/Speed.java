948404898a5abce03d4b0fe01ebd90f9a015e244
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-09-29 15:46:15 +0200

    Update Speed.java

--- src/main/java/me/derp/quantum/features/modules/movement/Speed.java
+++ src/main/java/me/derp/quantum/features/modules/movement/Speed.java
@@ -1,4 +1,4 @@
-package me.derp.quantum.features.modules.movement;
+package me.derp.quantum.features.modules.movement
 
 import me.derp.quantum.Quantum;
 import me.derp.quantum.features.modules.Module;
