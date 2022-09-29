5de5ee97737d1b9397d02159bc5ad6823de6ab5e
Rat2b2t <bigratmonsterr@gmail.com>
2022-09-29 22:16:53 +0800

    wtf is snitemode

--- src/main/java/me/derp/quantum/features/modules/misc/RPC.java
+++ src/main/java/me/derp/quantum/features/modules/misc/RPC.java
@@ -10,7 +10,7 @@ public class RPC
     public static RPC INSTANCE;
     public Setting<String> state = this.register(new Setting<>("State", "Quantum Continued v0.6.1", "Sets the state of the DiscordRPC."));
     public Setting<Boolean> showIP = this.register(new Setting<>("ShowIP", Boolean.TRUE, "Shows the server IP in your discord presence."));
-    public Setting<Boolean> catMode = this.register(new Setting<>("SnineMode", false, "cute cat supremacy"));
+    public Setting<Boolean> catMode = this.register(new Setting<>("CatMode", false, "cute cat supremacy"));
 
     public RPC() {
         super("RPC", "Discord rich presence", Module.Category.MISC, false, false, false);
