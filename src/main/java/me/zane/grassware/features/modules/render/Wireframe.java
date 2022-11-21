b32a4494d84526cfd9ea86a74fc660ad440695db
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-11-21 17:20:32 +0200

    Update Wireframe.java

--- src/main/java/me/zane/grassware/features/modules/render/Wireframe.java
+++ src/main/java/me/zane/grassware/features/modules/render/Wireframe.java
@@ -8,11 +8,16 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Wireframe
         extends Module {
     private static Wireframe INSTANCE = new Wireframe();
-    public final Setting<Float> alpha = this.register(new Setting<>("PAlpha", 255.0f, 0.1f, 255.0f));
-    public final Setting<Float> lineWidth = this.register(new Setting<>("PLineWidth", 1.0f, 0.1f, 3.0f));
-    public Setting<RenderMode> mode = this.register(new Setting<>("PMode", RenderMode.SOLID));
-    public Setting<Boolean> players = this.register(new Setting<>("Players", Boolean.FALSE));
-    public Setting<Boolean> playerModel = this.register(new Setting<>("PlayerModel", Boolean.FALSE));
+    public final Setting<Float> alpha = this.register(new Setting<Float>("PAlpha", Float.valueOf(255.0f), Float.valueOf(0.1f), Float.valueOf(255.0f)));
+//  public final Setting<Float> cAlpha = this.register(new Setting<Float>("CAlpha", Float.valueOf(255.0f), Float.valueOf(0.1f), Float.valueOf(255.0f)));
+    public final Setting<Float> lineWidth = this.register(new Setting<Float>("PLineWidth", Float.valueOf(1.0f), Float.valueOf(0.1f), Float.valueOf(3.0f)));
+//  public final Setting<Float> crystalLineWidth = this.register(new Setting<Float>("CLineWidth", Float.valueOf(1.0f), Float.valueOf(0.1f), Float.valueOf(3.0f)));
+    public Setting<RenderMode> mode = this.register(new Setting<RenderMode>("PMode", RenderMode.SOLID));
+//  public Setting<RenderMode> cMode = this.register(new Setting<RenderMode>("CMode", RenderMode.SOLID));
+    public Setting<Boolean> players = this.register(new Setting<Boolean>("Players", Boolean.FALSE));
+    public Setting<Boolean> playerModel = this.register(new Setting<Boolean>("PlayerModel", Boolean.FALSE));
+//  nampublic Setting<Boolean> crystals = this.register(new Setting<Boolean>("Crystals", Boolean.FALSE));
+//  public Setting<Boolean> crystalModel = this.register(new Setting<Boolean>("CrystalModel", Boolean.FALSE));
 
     public Wireframe() {
         super("Wireframe", "Draws a wireframe esp around other players.", Module.Category.RENDER, false, false, false);
@@ -41,4 +46,3 @@ public class Wireframe
 
     }
 }
-
